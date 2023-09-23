b5d813fabae719bb3f3c097c467566bc7a94439e
almasutami <86611956+almasutami@users.noreply.github.com>
2023-09-23 15:02:19 +0700

    feat: settings page responsivity

--- src/pages/employee/employee-settings.vue
+++ src/pages/employee/employee-settings.vue
@@ -4,18 +4,40 @@ import CardLayout from '@/components/container-layout.vue'
 import ProfileCard from '@/components/employee/profile-card.vue'
 import EmployeeTabMenu from '@/components/employee/tab-menu.vue'
 import { employeeStore } from '@/stores/employee'
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, onUnmounted, ref } from 'vue'
 
 const { selectedEmployee } = storeToRefs(employeeStore())
 const onFocus = () => {
   return 'focus:border-orange-500 focus:shadow-sm'
 }
-const socMedLabel = () => {
-  return 'rounded-l-md rounded-r-none bg-gray-100 border border-gray-400 col-span-2 col-start-1 px-2 sm:px-1 h-full items-center flex text-slate-800'
-}
-const socMedContainerLabel = () => {
-  return 'col-start-2 col-span-5 grid grid-cols-6 lf:col-start-1 md:col-start-1 sm:col-start-1 md:col-span-6 lg:col-span-6 sm:col-span-6 items-center'
-}
+
+const socmedContainerStyle = computed(() => {
+  return windowWidth.value > 375 ? 'grid grid-cols-6 items-center' : 'grid grid-cols-1 items-center'
+})
+
+const socMedTitleStyle = computed(() => {
+  return windowWidth.value > 375
+    ? 'col-start-1 col-span-1 lg:col-span-6 md:col-span-6 sm:col-span-6 font-semibold'
+    : 'col-start-1 col-span-1 font-semibold'
+})
+
+const socMedContainerLabelStyle = computed(() => {
+  return windowWidth.value > 375
+    ? 'col-start-2 col-span-5 grid grid-cols-6 lg:col-start-1 md:col-start-1 sm:col-start-1 md:col-span-6 lg:col-span-6 sm:col-span-6 items-center'
+    : 'grid grid-cols-12 col-start-1 col-span-1 items-center'
+})
+
+const socMedLabelStyle = computed(() => {
+  return windowWidth.value > 375
+    ? 'rounded-l-md rounded-r-none bg-gray-100 border border-gray-400 col-span-2 col-start-1 px-2 sm:px-1 h-full items-center flex text-slate-800'
+    : 'rounded-l-md rounded-r-none bg-gray-100 border border-gray-400 px-1 h-full items-center flex text-slate-800 col-start-1 col-span-6'
+})
+
+const socMedInputStyle = computed(() => {
+  return windowWidth.value > 375
+    ? 'w-full border-gray-400 rounded-r-md rounded-l-none col-span-4 col-start-3' + onFocus()
+    : 'w-full border-gray-400 rounded-r-md rounded-l-none col-span-6 col-start-7' + onFocus()
+})
 
 const twitterUrl = ref(selectedEmployee?.value?.social_media?.[0]?.twitter)
 const facebookUrl = ref(selectedEmployee?.value?.social_media?.[0]?.facebook)
@@ -35,6 +57,12 @@ onMounted(async () => {
   instagramUrl.value = selectedEmployee?.value?.social_media?.[0]?.instagram
   loading.value = false
 })
+
+let windowWidth = ref(window.innerWidth)
+
+const onWidthChange = () => (windowWidth.value = window.innerWidth)
+onMounted(() => window.addEventListener('resize', onWidthChange))
+onUnmounted(() => window.removeEventListener('resize', onWidthChange))
 </script>
 
 <template>
@@ -67,62 +95,47 @@ onMounted(async () => {
           <div class="h-full py-4">
             <div class="font-semibold px-4 text-base">Sosial Media</div>
             <div class="w-full flex flex-col gap-3 py-5 px-4">
-              <div class="grid grid-cols-6 items-center">
-                <div
-                  class="col-start-1 col-span-1 lg:col-span-6 md:col-span-6 sm:col-span-6 font-semibold"
-                >
-                  Instagram
-                </div>
-                <div :class="socMedContainerLabel()">
-                  <div :class="socMedLabel()">instagram.com/</div>
+              <div :class="socmedContainerStyle">
+                <div :class="socMedTitleStyle">Instagram</div>
+                <div :class="socMedContainerLabelStyle">
+                  <div :class="socMedLabelStyle">instagram.com/</div>
                   <InputText
                     v-if="!loading"
-                    :class="onFocus()"
                     v-model="instagramUrl"
-                    class="w-full border-gray-400 rounded-r-md rounded-l-none col-span-4 col-start-3"
+                    :class="socMedInputStyle"
                     placeholder="Username facebook"
                   ></InputText>
-                  <div v-if="loading" :class="socMedContainerLabel()">
+                  <div v-if="loading" :class="socMedContainerLabelStyle">
                     <Skeleton class="h-[40px]" />
                   </div>
                 </div>
               </div>
-              <div class="grid grid-cols-6 items-center">
-                <div
-                  class="col-start-1 col-span-1 lg:col-span-6 md:col-span-6 sm:col-span-6 font-semibold"
-                >
-                  Twitter
-                </div>
-                <div :class="socMedContainerLabel()">
-                  <div :class="socMedLabel()">twitter.com/</div>
+              <div :class="socmedContainerStyle">
+                <div :class="socMedTitleStyle">Twitter</div>
+                <div :class="socMedContainerLabelStyle">
+                  <div :class="socMedLabelStyle">twitter.com/</div>
                   <InputText
                     v-if="!loading"
                     v-model="twitterUrl"
-                    :class="onFocus()"
-                    class="w-full border-gray-400 rounded-r-md rounded-l-none col-span-4 col-start-3"
+                    :class="socMedInputStyle"
                     placeholder="Username twitter"
                   ></InputText>
-                  <div v-if="loading" :class="socMedContainerLabel()">
+                  <div v-if="loading" :class="socMedContainerLabelStyle">
                     <Skeleton class="h-[40px]" />
                   </div>
                 </div>
               </div>
-              <div class="grid grid-cols-6 items-center">
-                <div
-                  class="col-start-1 col-span-1 lg:col-span-6 md:col-span-6 sm:col-span-6 font-semibold"
-                >
-                  Facebook
-                </div>
-                <div :class="socMedContainerLabel()">
-                  <div :class="socMedLabel()">facebook.com/</div>
+              <div :class="socmedContainerStyle">
+                <div :class="socMedTitleStyle">Facebook</div>
+                <div :class="socMedContainerLabelStyle">
+                  <div :class="socMedLabelStyle">facebook.com/</div>
                   <InputText
                     v-if="!loading"
                     v-model="facebookUrl"
-                    :class="onFocus()"
-                    class="w-full border-gray-400 rounded-r-md rounded-l-none col-span-4 col-start-3"
+                    :class="socMedInputStyle"
                     placeholder="Username facebook"
                   ></InputText>
-                  <div v-if="loading" :class="socMedContainerLabel()">
+                  <div v-if="loading" :class="socMedContainerLabelStyle">
                     <Skeleton class="h-[40px]" />
                   </div>
                 </div>
